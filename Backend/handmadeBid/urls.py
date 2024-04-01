5f0c8c7dbb4c44734da6c47a24f3ecea25eac92f
vickivv <115426094+vickivv@users.noreply.github.com>
2024-03-31 17:21:23 -0700

    Revert "pull from olivia: Add Frontend, implemented login signup feature"

--- Backend/handmadeBid/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-from .views import LoginView
-
-urlpatterns = [
-    path('signup/', views.signup, name='signup'),
-    path('login/', LoginView.as_view(), name='login'),
-    path('admin/login/', LoginView.as_view(), name='admin_login'),
-]
--- Frontend/bidheaven/src/Components/Messages/Modals.js
+++
@@ -1,9 +0,0 @@
-import React,{useState} from 'react'
-import {useForm} from 'react-hook-form'
-import{Modal,messagge}from 'antd'
-import axios from '../axios/axios'
-
-function Modal (props){
-
-}
-export default Modals
